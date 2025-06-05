096e584ef40de2e19f00f7bca88c67a4fce2843e
Marcos <you@example.com>
2025-06-05 18:10:23 -0300

    initial commit

--- gerar_senha.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-async function gerarSenha() {
-  const senha = "Teste123!"; // Senha de teste
-  const hash = await bcrypt.hash(senha, 10);
-  console.log("Senha hash:", hash);
-}
-
-gerarSenha();
