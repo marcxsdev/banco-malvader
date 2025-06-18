19b5709c3ce4c969a09d8d313c522411ea4edffa
marcxsdev <marcosalexandre1322@gmail.com>
2025-06-18 04:09:01 -0300

    fix: fix HeaderCliente logout button

--- src/app/components/HeaderCliente.jsx
+++ src/app/components/HeaderCliente.jsx
@@ -2,6 +2,10 @@ import Link from "next/link";
 import Image from "next/image";
 
 const HeaderCliente = () => {
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+  };
+
   return (
     <div className="flex flex-row items-center justify-between px-26 py-4 border-b ">
       <Image
@@ -12,7 +16,10 @@ const HeaderCliente = () => {
         className="hover:scale-110 transition duration-300 cursor-pointer"
       />
       <Link href="/pages/login">
-        <button className="bg-black flex flex-row items-center justify-center gap-4 py-2.5 px-8 max-h-[45px] rounded-xl cursor-pointer hover:bg-neutral-800 hover:scale-105 transition duration-300">
+        <button
+          onClick={handleLogout}
+          className="bg-black flex flex-row items-center justify-center gap-4 py-2.5 px-8 max-h-[45px] rounded-xl cursor-pointer hover:bg-neutral-800 hover:scale-105 transition duration-300"
+        >
           <span className="text-white font-bold">Sair</span>{" "}
         </button>
       </Link>
